c53fd3b302905629c69163068803ec94844ae2ac
Samad <Samad.o@outlook.com>
2025-12-30 17:42:43 -0500

    project on hold until I learn more about apis

---
+++ script.js
@@ -0,0 +1,5 @@
+const day = document.querySelector('day');
+const date = document.querySelector('date');
+const location = document.querySelector('location');
+const weather = document.querySelector('weather');
+const weatherConditions = document.querySelector('weather__conditions');
