112ee7b2168ce36b505895376c4d661c6b97017e
Samad <Samad.o@outlook.com>
2026-01-18 16:25:52 -0500

    Basic API functionality configured

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const api = `http://api.openweathermap.org/data/2.5/weather?q=Summerfield&appid=9cdfbf00a3ee210870169cb693a829c9&units=metric`;
+const apiKey = "9cdfbf00a3ee210870169cb693a829c9";
+let city = "New York";
+const apic = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-console.log(api)
+const apif = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+
+fetch(apic)
+  .then((res) => res.json())
+  .then((data) => {
+
+    const weather = document.getElementById('weatherc')
+    weather.innerText = data.main.temp + " C"
+  });
+
+
+  fetch(apif)
+  .then((res) => res.json())
+  .then((data) => {
+
+    const weather = document.getElementById('weatherf');
+    weather.innerText = data.main.temp + " F";
+  });
+
+fetch (apif).then(res => console.log(res.json()));
+
