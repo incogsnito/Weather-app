90490103730c82cd932b68ddd6373675f83b6d95
Samad <Samad.o@outlook.com>
2026-01-18 18:54:13 -0500

    Added styling base config complete

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ const apiKey = "9cdfbf00a3ee210870169cb693a829c9";
 let city = "New York";
 const apic = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-const apif = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+const apif = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 fetch(apic)
   .then((res) => res.json())
   .then((data) => {
-
-    const weather = document.getElementById('weatherc')
-    weather.innerText = data.main.temp + " C"
+    const weather = document.getElementById("weatherc");
+    weather.innerText = data.main.temp + " C";
   });
 
-
-  fetch(apif)
+fetch(apif)
   .then((res) => res.json())
   .then((data) => {
-
-    const weather = document.getElementById('weatherf');
+    const weather = document.getElementById("weatherf");
     weather.innerText = data.main.temp + " F";
   });
 
-fetch (apif).then(res => console.log(res.json()));
+fetch(apif)
+  .then((res) => res.json())
+  .then((data) => {
+    const desc = document.getElementById("description");
+    desc.innerText = data.weather[0].description; //basically changes the inner text of the desc to the api's
+  });
 
+fetch(apif)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  });
